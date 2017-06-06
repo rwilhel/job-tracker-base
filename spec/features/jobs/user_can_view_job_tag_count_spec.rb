352e28402e213c796d86b144184123cfe281a254
Rhonda <rwilhel@gmail.com>
2017-06-06 11:03:38 -0600

    Creates feature test for user can view how many jobs are associated with a tag

---
+++ spec/features/jobs/user_can_view_job_tag_count_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe "User can view how many jobs have listed tags" do
+  scenario "can see the number of tags that are associated with jobs" do
+    company = Company.create!(name: "ESPN")
+    job = company.jobs.create!(title: "Developer", level_of_interest: 70, city: "Denver")
+    tag_1 = job.tags.create!(name: "Software")
+    tag_2 = job.tags.create!(name: "Good-Location")
+
+    visit company_job_path(company, job)
+
+    expect(page).to have_content(company.name)
+    expect(page).to have_content(job.title)
+    expect(page).to have_content(tag_1.name)
+    expect(page).to have_content(tag_2.name)
+  end
+end
