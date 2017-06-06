acace112e12455e43ebb5b2a83e257a3a3e946b2
Rhonda <rwilhel@gmail.com>
2017-06-06 10:12:32 -0600

    Creates feature test for user can view all tags on a specific job

---
+++ spec/features/jobs/user_can_view_tags_on_a_job_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+Rspec.describe "User can view all tags on a specific job" do
+  scenario "can see the name of the tags associated with the job" do
+    company = Company.create!(name: "ESPN")
+    job = company.jobs.create!(title: "Developer", level_of_interest: 70, city: "Denver")
+    tag = job.tags.create!(name: "Software")
+
+    visit company_job_path(company, job)
+
+    expect(page).to have_content("ESPN")
+    expect(page).to have_content("Developer")
+    expect(page).to have_content("Software")
+  end
+end
